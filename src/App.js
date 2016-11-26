2a942c00e56f4eb83f10c3a74983a96f5f3bb759
Steven Shao <stevenshao1900@icloud.com>
2016-11-26 14:44:25 +0800

    layout change & use react-router

--- src/App.js
+++ src/App.js
@@ -45,26 +45,31 @@ class WelcomeInfo extends Component {
   render() {
     return (
       <div>
-        <div> {"预约项目说明"} </div>
-        <div> {"预约成功后订单不可更改，如需取消将产生订金损失，退款规则如下："} </div>
-        <div> {"距培训时间72小时以上取消订单，退99%订金"} </div>
-        <div> {"距培训时间48-72小时取消订单，退50%订金"} </div>
-        <div> {"距培训时间不足48小时取消订单，不可退订金"} </div>
+        <div style={{textAlign:"left",marginLeft:"20px"}}> <h5>{"Terms of Booking"}</h5></div>
+        <div style={{textAlign:"left", marginLeft:"30px", marginBottom:"5px", marginRight:"30px"}}> {"*预约成功后订单不可更改，如需取消将产生订金损失，退款规则如下："} </div>
+        <div style={{textAlign:"left", marginLeft:"34px", marginRight:"30px"}}> {"距培训时间72小时以上取消订单，退99%订金"} </div>
+        <div style={{textAlign:"left", marginLeft:"34px", marginRight:"30px"}}> {"距培训时间48-72小时取消订单，退50%订金"} </div>
+        <div style={{textAlign:"left", marginLeft:"34px", marginRight:"30px"}}> {"距培训时间不足48小时取消订单，不可退订金"} </div>
 
       </div>
     )
   }
 }
 
-class WelcomeTitle extends Component {
+class WelcomeTitle extends Component { //Welcome Who?
   render() {
-    return <div>{"Welcome Mr. Steven"}</div>;
+    return <div><h5>{"Welcome " + this.props.userName +" to Emotion Studio"}</h5></div>;
   }
 }
 
 class WelcomeStepInfo extends Component {
   render() {
-    return <div>{"Steps: 1. Select coach; 2. Select Date; 3. Select Time; 4. Pay the deposite."}</div>;
+    return (
+        <div>
+            <div style={{textAlign:"left",marginLeft:"20px"}}><h5>{"Booking Steps"}</h5></div>
+            <div style={{textAlign:"left", marginLeft:"34px", marginRight:"30px"}}>{"1 Select coach 2 Select Date 3 Select Time 4 Pay the deposite"}</div>
+        </div>
+    )
   }
 }
 
@@ -82,15 +87,18 @@ class WelcomePage extends Component {
   render() {
     return (
       <div>
-        <WelcomeTitle />
+        <WelcomeTitle userName="James Chen"/>
+        <div className="lineSeparator" />
         <WelcomeInfo />
+        <div className="lineSeparator" />
         <WelcomeStepInfo />
+
         <div>
-          {/*this is not good.*/}
-          <Button onClick={this.goToNextPage}>{"开始预约"}</Button>
+
+          <Button onClick={this.goToNextPage} style={{width:"120px",margin:"10px"}}>{"Start Booking"}</Button>
         </div>
         <div>
-          <Button>{"我的预约"}</Button>
+          <Button style={{width:"120px"}} >{"My Bookings"}</Button>
         </div>
       </div>
 
